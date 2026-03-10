1633fac40e2a6438225a986e7fb920c6a8f81f60
BB14official <benedikt.beck.mueb@t-online.de>
2026-03-10 12:45:04 +0100

    Update xG.py

--- xG.py
+++ xG.py
@@ -40,7 +40,7 @@ font_props = load_font()
 teams["color"] = ["#AA1124", "#F8D615", "#CD1719", "#ED1248", "#006BB3", "#C20012", "#E3191B", "#03466A", 
                   "#2FA641", "#009C6B", "#ED1B24", "#E3000F", "#2E438C", "#5AAADF", "#EE232B"]
 
-theme = st.get_option("theme.base") 
+mode = st.sidebar.toggle("Dark Mode", value=st.session_state.theme == "dark")
 
 if theme == "Dark": 
     background_color = "#262730" 
@@ -1133,6 +1133,7 @@ else:
 
 
     plt.close(fig)
+
 
 
 
